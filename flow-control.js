84e10935207089dfe1ce6a8bc46000a2946e337d
dila oral <zeynepdilaoral1@gmail.com>
2016-08-12 15:27:50 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,34 @@
 function basicTeenager(age) {
-
-}
+ if(ageIsBetween13And19) }
+  "You are a teenager!" ()
 
 function teenager(age) {
-
+ if (ageIsBetween13And19) {
+ "You are a teenager!" ()
+} else {
+  "You are not a teenager!" ()
 }
 
 function ageChecker(age) {
-
+if (ageIsBetween13And19)
+"You are a teenager!" ()
+} else if (ageIsBetween12OrBelow) {
+  "You are a kid!" ()
+} else {
+  "You are a grownup" ()
 }
 
 function ternaryTeenager(age) {
-
+  YouAreATeenager(ageIsBetween13And19)
+  return true
+if (ageIsNotBetween13and19) {
+  return false
 }
 
 function switchAge(age) {
+switch (ageIsBetween13And19) {
+  case expression: "You are a teenager"
 
+    break;
+  default: "You are not a teenager"
 }
