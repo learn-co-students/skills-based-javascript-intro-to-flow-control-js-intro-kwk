6e7ce06187c73f0d8fcfacb798370ecb3d5dcc47
Ailsa Meechan <rainbows_and_oranges@live.co.uk>
2016-08-14 21:53:29 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,8 @@
-function basicTeenager(age) if((age >= 13) && (age <= 19))
-  {return ("You are a teenager!")}
-  else {return ("You are not a teenager")}}
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
+}
 
 
 
