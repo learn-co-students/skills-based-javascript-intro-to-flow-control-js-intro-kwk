f26e797c1fee0bd77fe041879aa07744c3bbe252
Ailsa Meechan <rainbows_and_oranges@live.co.uk>
2016-08-15 10:13:53 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -23,30 +23,25 @@ function ternaryTeenager(age) {
   } else {return "You are not a teenager"}
   }
 
-function switchAge(age) {
-  switch(age) {
-    case "13":
-    console.log("You are a teenager");
-    break;
-    case "14":
-    console.log("You are a teenager");
-    break;
-    case "15":
-    console.log("You are a teenager");
-    break;
-    case "16":
-    console.log("You are a teenager");
-    break;
-    case "17":
-    console.log("You are a teenager");
-    break;
-    case "18":
-    console.log("You are a teenager");
-    break;
-    case "19":
-    console.log("You are a teenager");
-    break;
-    default:
-    console.log("You have an age");
-  }
-}
+function switchAge(age) {switch(age) {case "13": console.log("You are a teenager");
+break;
+case "14":
+console.log("You are a teenager");
+break;
+case "15":
+console.log("You are a teenager");
+break;
+case "16":
+console.log("You are a teenager");
+break;
+case "17":
+console.log("You are a teenager");
+break;
+case "18":
+console.log("You are a teenager");
+break;
+case "19":
+console.log("You are a teenager");
+break;
+default:
+console.log("You have an age")}}
