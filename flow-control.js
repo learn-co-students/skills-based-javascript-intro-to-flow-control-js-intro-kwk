dec8de13e4004f44c68b8b8a5038976172e7a3d5
ALESLIANA WIRTH <lesli.wirth16@gmail.com>
2016-09-03 18:32:01 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ else {
 return "You are a grownup"}
 }
 
-function ternaryTeenager(age) { (age >=13 && age <=19) ?
+function ternaryTeenager(age) { return (age >=13 && age <=19) ?
 "You are a teenager" : "You are not a teenager";
 }
 
