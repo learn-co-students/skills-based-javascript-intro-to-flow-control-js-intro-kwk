1485c6a7038564e679a7c7453e993bc415fb963b
Victoria Chiu <victoria@chiucompany.com>
2016-09-20 03:08:23 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,15 @@
 function basicTeenager(age) {
-  if (age >= 13 && age <= 19)
-{
-      return "You are a teenager!";
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
   }
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return 'You are a teenager!'
+    return "You are a teenager!"
 } else {
-  return 'You are not a teenager'
-}
+  return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
@@ -18,20 +17,19 @@ function ageChecker(age) {
     return "You are a teenager!"
 } else if (age < 13) {
   return "You are a kid"
-} else (age >19) {
+} else if (age > 19) {
   return "You are a grownup"
-}
+  }
 }
 
 function ternaryTeenager(age) {
-  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
+  return (age >= 13 && age <= 19) ? "You are a teenager":"You are not a teenager"
 }
 
 function switchAge(age) {
-switch(age) {
-  case age >= 13 && age <= 19:
-  return "You are a teenager";
-  default:
-  return "You have an age";
-}
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager"
+  } else {
+    return "You have an age"
+  }
 }
