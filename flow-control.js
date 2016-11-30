c8dea219ee1accde43e1a087536da3760fd1420f
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-11-30 23:46:07 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -24,9 +24,9 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if age >= 13 && age <= 19)?
+  age >= 13 && age <= 19 ?
   'You are a teenager!':
-  'You are not a teenager';
+  'You are not a teenager'
   }
 
 
