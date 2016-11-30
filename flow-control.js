dfb9abd8dbbf0d10528cd5034706f20dea003ecc
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-11-30 22:24:33 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,9 +15,9 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19){
-    console.log("You are a teenager!")
+    return "You are a teenager!"
   } else if (age <= 12){
-    console.log("You are a kid")
+    return "You are a kid"
   } else {
     return "You are a grownup"
   }
