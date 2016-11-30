54cedfeee89cb11209512792d6a58640ff38b2dc
Kimia Kalantari <kimiakalantari1356@gmail.com>
2016-11-30 22:48:05 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -31,31 +31,31 @@ function ternaryTeenager(age) {
   }
 }
 
-var age = 19
+
 function switchAge(age) {
   switch(age) {
   case 13:
-  "You are a teenager!";
+  console.log("You are a teenager!");
   break;
   case 14:
-  "You are a teenager!";
+  console.log("You are a teenager!");
   break;
   case 15:
-  "You are a teenager!";
+  console.log("You are a teenager!");
   break;
   case 16:
-  "You are a teenager!";
+  console.log("You are a teenager!");
   break;
   case 17:
-  "You are a teenager!";
+  console.log("You are a teenager!");
   break;
   case 18:
-  "You are a teenager!";
+  console.log("You are a teenager!");
   break;
   case 19:
-  "You are a teenager!";
+  console.log("You are a teenager!");
   break;
   default:
-  "You have an age";
+  return "You have an age"
 }
 }
