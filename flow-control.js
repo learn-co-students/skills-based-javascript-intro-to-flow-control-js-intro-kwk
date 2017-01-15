301bb96c2e8599cb4688168a674650a0d08a4c2c
Sibel Unal <sibelunal82@gmail.com>
2017-01-15 13:08:01 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -11,7 +11,7 @@ function ageChecker(age) {if (age>=13 && age<=19) {return "You are a teenager!"}
 
 }
 
-function ternaryTeenager(age) {
+function ternaryTeenager(age) {if (age<=19 && age>=13) {return "You are a teenager!" } else {return "You are not a teenager!"}
 
 }
 
