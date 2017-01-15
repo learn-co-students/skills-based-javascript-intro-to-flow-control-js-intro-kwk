ba6a28ee63789c9753ad5f9c70dc33eaf79b8c77
Sibel Unal <sibelunal82@gmail.com>
2017-01-15 12:59:57 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -6,7 +6,8 @@ function teenager(age) {
 
 }
 
-function ageChecker(age) {
+function ageChecker(age) {if (age>=13 && age<=19) {return "You are a teenager!"} else if (age<=12) {return "You are a kid!"} return "You are a grownup!"}
+
 
 }
 
