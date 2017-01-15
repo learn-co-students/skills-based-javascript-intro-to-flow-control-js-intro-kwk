625c6afaed214eef905eed656005aa737fdc2145
Sibel Unal <sibelunal82@gmail.com>
2017-01-15 13:14:21 +0000

    Done.

--- flow-control.js
+++ flow-control.js
@@ -15,6 +15,7 @@ function ternaryTeenager(age) {if (age<=19 && age>=13) {return "You are a teenag
 
 }
 
-function switchAge(age) {
+function switchAge(age) {switch(age){case 13 || 14 || 15 || 16 || 17 || 18 || 19 : console.log("You are a teenager"); break; default: console.log("You have an age");}
+
 
 }
